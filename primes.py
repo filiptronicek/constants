4e960e3ff305a4e3cddc456a8f7d371cce1b5346
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-10 20:16:52 +0000

    Primes script and some primes

--- primes.py
+++ primes.py
@@ -1,10 +1,21 @@
+import atexit
+
 primes = []
 
+def writeToFile():
+    with open("outputs/primes.txt", "w") as f:
+        f.write('\n'.join(primes))
+
 i = 1
-while True:
-    i += 1
-    for d in range(2,i):
-        if i % d == 0: break
-    else:
-        primes.append(i)
-        print(i)
+try:
+    while True:
+        i += 1
+        for d in range(2,i):
+            if i % d == 0: break
+        else:
+            primes.append(str(i))
+            print(i)
+except KeyboardInterrupt:
+    writeToFile()
+
+atexit.register(writeToFile)
